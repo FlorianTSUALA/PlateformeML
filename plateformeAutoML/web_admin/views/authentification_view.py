e6b4128bd252ddebf288ab4b25279e95796f8ee5
Prodigit <floriantsuala@yahoo.fr>
2022-03-14 00:52:38 +0100

    auth template

--- plateformeAutoML/web_admin/views/authentification_view.py
+++ plateformeAutoML/web_admin/views/authentification_view.py
@@ -36,3 +36,60 @@ def connexion(request):
     else:
         return render(request, 'users/login.html')
     return render(request,'users/login.html')
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password') 
+        print(username)
+        print(password)
+        
+        user = authenticate(request,username=username,password=password)
+        print(user)
+        if user is not None:
+            login(request,user)
+            return redirect('dashboard')
+        else:
+            messages.info(request, 'Username or password is not correct')    
+    return render(request, 'login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('dashbord')
+
+#def register(request):
+ #   form = CreateUser()
+  #  if request.method == 'POST':
+   #     form = CreateUser(request.POST)
+    #    if form.is_valid():
+     #       form.save()
+      #      user=form.cleaned_data.get('username')
+       #     messages.success(request, 'Votre compte a été creer.' + user)
+        #    return redirect('login')
+        #else:
+         #   print('invalide data')
+        #form = CreateUser()
+
+    #context = {'form': form}
+    #return render(request, 'register.html',context)
+
+def register(request):
+    form = CreateUser()
+    if request.method == 'POST':
+        user = User()
+        form = CreateUser(request.POST)
+        username = request.POST.get('username')
+        print(form)
+        if form.is_valid():
+            form.save()
+            user=form.cleaned_data.get('username')
+            messages.success(request, 'Votre compte a été creer.' + user)
+            return redirect('login')
+            print('ok')
+        else:
+            print('invalide data')
+           # form = CreateUser()
+
+    context = {'form': form}
+    return render(request, 'register.html',context)
