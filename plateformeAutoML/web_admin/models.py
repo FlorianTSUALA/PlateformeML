05010279002d568601e67e8781b5e900043f3d03
jauresimo98 <75648029+jauresimo98@users.noreply.github.com>
2022-03-21 09:54:18 +0100

    create_user

--- plateformeAutoML/web_admin/models.py
+++ plateformeAutoML/web_admin/models.py
@@ -29,7 +29,9 @@ class Utilisateur(models.Model):
     prenom = models.CharField(max_length=70, blank=True,null=True)
     telephone = models.CharField(max_length=254, blank=True,null=True)
     pays = models.CharField(max_length=254, blank=True,null=True)
+    ville = models.CharField(max_length=254, blank=True,null=True)
     compte = models.OneToOneField( Compte, on_delete=models.CASCADE, primary_key=True,)
+    decription = models.TextField(blank=True,null=True)
 
     def __str__(self):
         return "%s  %s" % (self.prenom, self.nom)
--- plateformeAutoML/web_admin/views/utilisateur_view.py
+++ plateformeAutoML/web_admin/views/utilisateur_view.py
@@ -5,6 +5,32 @@ from django.views.generic import TemplateView, View, DeleteView, ListView, Updat
 from django.core import serializers
 from django.http import JsonResponse
 
+#######
+
+import email
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import TemplateView, View, DeleteView, ListView, UpdateView
+from django.core import serializers
+from django.http import JsonResponse
+from django.contrib.auth import (authenticate, login, logout, get_user_model, REDIRECT_FIELD_NAME)
+from django.core.exceptions import ObjectDoesNotExist
+import hashlib, binascii
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login as auth_login
+from django.contrib import messages
+from django.contrib.auth.models import User,Group
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+from web_admin.models import Compte, Utilisateur
+from django.conf import settings
+
 class MonCompteView(TemplateView):
     template_name = 'pages/utilisateurs/mon_compte.html'
 
@@ -25,6 +51,58 @@ class CreateUtilisateurView(TemplateView):
         context['section_title'] = 'Utilisateur'
         context['section_item_title'] = 'Mon Compte'
         return context
+
+
+def register(request):
+    print('registration')
+    if request.method == "POST":
+        login = request.POST.get('login')
+        email = request.POST.get('email')
+        pwd1 = request.POST.get('pwd1')
+        pwd2 = request.POST.get('pwd2')
+        
+        nom = request.POST.get('nom')
+        prenom = request.POST.get('prenom')
+        telephone = request.POST.get('telephone')
+        pays = request.POST.get('pays')
+        ville = request.POST.get('ville')
+        description = request.POST.get('description')
+
+        if pwd1 !=pwd2:
+            message = 'Les mots de passes ne sont pas identiques'
+            return render(request,'pages/authentification/register.html',{'message':message})
+        else:
+           dk = hashlib.pbkdf2_hmac('sha256', str.encode(pwd1), b'salt', 10000)
+           password = binascii.hexlify(dk)
+           compte = Compte(
+                            login = login,
+                            email = email,
+                            password = password
+                        )
+           compte.save()
+
+           utilisateur = Utilisateur(
+                            nom = nom,
+                            prenom = prenom,
+                            telephone = telephone,
+                            pays = pays,
+                            compte = compte,
+                            description = description,
+                            ville = ville
+                        )
+           utilisateur.save()
+
+           return redirect(settings.LOGIN_URL)
+
+            # messages.error(request, 'creation de compte échouée')
+            # render(request,'users/register.html',{'form':form})
+    else:
+        return render(request, 'pages/utilisateur/create_utilisateur.html')
+        
+    return render(request,'pages/utilisateur/create_utilisateur.html')
+
+
+
 
 
 class ListUtilisateurView(TemplateView):
