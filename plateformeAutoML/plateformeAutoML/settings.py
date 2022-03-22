0f3e7732920583f1cc39e71ca22318006f38a3f5
Prodigit <floriantsuala@yahoo.fr>
2022-03-22 23:00:16 +0100

    prototype des la creation des projet

--- plateformeAutoML/plateformeAutoML/settings.py
+++ plateformeAutoML/plateformeAutoML/settings.py
@@ -97,6 +97,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #On Docker ENV
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get('POSTGRES_NAME'),
+    #     'USER': os.environ.get('POSTGRES_USER'),
+    #     'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+    #     'HOST': 'db',
+    #     'PORT': 5432,
+    # }
 }
 
 
