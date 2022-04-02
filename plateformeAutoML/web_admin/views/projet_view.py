68d094e93cfe5c2546c4ab2c80df731846bb91d2
Prodigit <floriantsuala@yahoo.fr>
2022-04-02 04:07:33 +0200

    update

--- plateformeAutoML/web_admin/views/projet_view.py
+++ plateformeAutoML/web_admin/views/projet_view.py
@@ -13,35 +13,6 @@ from web_admin.models import Fichier
 # Create your views here.
 from .utils_local import *
 
-
-
-ALLOWED_EXTENSIONS = set(["npy", "csv", "xls", "xlsx"])
-
-def save_info_projet(request):
-    if request.method == "POST":
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-        metrique = request.POST.get('metrique')
-        type = request.POST.get('type')
-        est_publique = request.POST.get('est_publique')
-        nombre_modele = request.POST.get('nombre_modele')
-    from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, View, DeleteView, ListView, UpdateView
-from django.core import serializers
-from django.http import JsonResponse
-from django.conf import settings
-import time, os
-
-from web_admin.models import Algorithme
-import pandas as pd
-from web_admin.models import Fichier
-
-# Create your views here.
-from .utils_local import *
-
-
-
 ALLOWED_EXTENSIONS = set(["npy", "csv", "xls", "xlsx"])
 
 def save_info_projet(request):
@@ -72,15 +43,187 @@ def save_info_projet(request):
         request.session['projet_id'] = id
         return JsonResponse({'data':{'id': id, 'msg':'Information enregistré avec success'}})
     
+# def upload_dataset(request):
+#     ts = time.gmtime()
+#     ts = time.strftime("__%Y_%m_%d__%H_%M_%S", ts)
+
+#     if request.method == 'POST':  
+#         fichier = request.FILES['file'].read()
+#         ext = os.path.splitest(request.POST['filename'])
+#         nom_fichier = ext[0] + ts + ext[1] 
+#         print('ext')
+#         print(ext[0])
+#         print(ext[1])
+#         print(ext)
+#         print(nom_fichier)
+#         print(nom_fichier)
+#         chemin = request.POST['path']
+#         end = request.POST['end']
+#         nextSlice = request.POST['nextSlice']
+#         if fichier=="" or nom_fichier=="" or chemin=="" or end=="" or nextSlice=="":
+#             res = JsonResponse({'data':'Requete invalide'})
+#             return res
+#         else:
+#             if chemin == 'null':
+#                 path = 'media/' + nom_fichier
+#                 with open(path, 'wb+') as destination: 
+#                     destination.write(fichier)
+#                 FileFolder = Fichier()
+#                 FileFolder.chemin = nom_fichier 
+#                 FileFolder.eof = end
+#                 FileFolder.nom = nom_fichier
+#                 FileFolder.save()
+
+#                 old_file_id = request.session.get('fichier_id', None)
+#                 if old_file_id is not None:
+#                     filename = Fichier.objects.get(pk=old_file_id).nom
+
+#                     media_root = getattr(settings, 'MEDIA_ROOT', None)
+#                     path_file = os.path.join(media_root, filename)
+#                     if os.path.isfile(path_file):
+#                         os.remove(path_file)
+
+#                     Fichier.objects.filter(id=old_file_id).delete()
+#                 request.session['fichier_id'] = FileFolder.pk
+                
+#                 if int(end):
+#                     data = info_dataset(path)
+#                     res = JsonResponse({'msg':'Chargment effectué avec success','data': data, 'chemin': chemin})
+#                 else:
+#                     res = JsonResponse({'chemin': nom_fichier})
+#                 return res
+
+#             else:
+#                 path = 'media/' + chemin
+#                 model_id = Fichier.objects.get(chemin=chemin)
+#                 if model_id.nom == nom_fichier:
+#                     if not model_id.eof:
+#                         with open(path, 'ab+') as destination: 
+#                             destination.write(fichier)
+#                         if int(end):
+#                             model_id.eof = int(end)
+#                             model_id.save()
+#                             data = info_dataset(model_id.chemin)
+#                             res = JsonResponse({'msg':'Chargement effectué avec success','data': data, 'chemin': model_id.chemin})
+#                         else:
+#                             res = JsonResponse({'chemin':model_id.chemin})    
+#                         return res
+#                     else:
+#                         res = JsonResponse({'data':'EOF trouvé. Requeste invalide'})
+#                         return res
+#                 else:
+#                     res = JsonResponse({'data':'Aucun fichier existant dans ce fichier'})
+#                     return res
+#     else:
+#         res = JsonResponse({'data':'Requete non authorisée'})
+#         return res
+
+
+# #FONCTION EXTRACTION DES CARACTERISQUE D'UNE COLONNE
+# def infos_dataset(dataframe):
+
+#     columns = dataframe.columns
+#     liste_col = {}
+#     for col in columns:
+#         macolonne = {}
+#         macolonne["type"] = dataframe[col].dtype
+#         macolonne["data"] = list(dataframe[col])
+
+#         if (dataframe[col].dtype == "int64" or dataframe[col].dtype == "int32"):
+#             macolonne["scaler"] = "Standard_Scaler"
+#             macolonne["imputer"] = "Mean"
+#             macolonne["encoder"] = "None"
+#             if dataframe[col].count() < 10:
+#                 macolonne["nature"] = "discret"
+#             else:
+#                 macolonne["nature"] = "continue"
+
+#         elif (dataframe[col].dtype == "bool"):
+#             macolonne["scaler"] = "None"
+#             macolonne["imputer"] = "Most_frequent"
+#             macolonne["encoder"] = "OneHot_Encoder"
+
+#             macolonne["nature"] = "discret"
+#         else:
+#             macolonne["scaler"] = "None"
+#             macolonne["imputer"] = "Most_frequent"
+#             macolonne["encoder"] = "OneHot_Encoder"
+
+#             macolonne["nature"] = "categoriel"
+
+#         liste_col[col] = macolonne
+
+#     return liste_col
+
+
+class MesFavorisView(TemplateView):
+    template_name = 'pages/projets/mes_favoris.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['has_white_text'] = False
+        context['section_title'] = 'Mon espace'
+        context['section_item_title'] = 'Mes favoris'
+        return context
+
+class ModifierProjetView(TemplateView):
+    template_name = 'pages/projets/detail_projet.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['has_white_text'] = False
+        context['section_title'] = 'Projets'
+        context['section_item_title'] = 'Mise à jour projet'
+        return context
+
+
+class ListeProjetView(TemplateView):
+    template_name = 'pages/projets/liste_projet.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['has_white_text'] = False
+        context['section_title'] = 'Projets'
+        context['section_item_title'] = 'Listes des projets'
+        return context
+        id = request.POST.get('id', 0)
+        if id == 0:
+            projet = Projet(
+                        title = title,
+                        description = description,
+                        metrique = metrique,
+                        type = type,
+                        est_publique = est_publique,
+                        nombre_modele = nombre_modele,
+                    )
+            projet.save()
+            id = projet.pk
+        else:
+            projet = Projet.objects.get(pk=id)
+            #update projet info
+        
+        request.session['projet_id'] = id
+        return JsonResponse({'data':{'id': id, 'msg':'Information enregistré avec success'}})
+    
+
+############################################################    SESSION
+
+def clean_session_project_creation(request):
+    request.session.clear()
+    #del request.session['key']
+
+def clean_session(request):
+    request.session.clear()
+    # del request.session['key']
+
 def upload_dataset(request):
     ts = time.gmtime()
     ts = time.strftime("__%Y_%m_%d__%H_%M_%S", ts)
-
     if request.method == 'POST':  
         fichier = request.FILES['file'].read()
-        ext = os.path.splitest(request.POST['filename'])
-        nom_fichier = ext[0] + ts + ext[1] 
-        print(nom_fichier)
+        nom_fichier = request.POST['filename']
+        ext = file_extention(nom_fichier)
+        nom_fichier = str(nom_fichier).replace(ext, '') + ts + ext
         chemin = request.POST['path']
         end = request.POST['end']
         nextSlice = request.POST['nextSlice']
@@ -98,11 +241,11 @@ def upload_dataset(request):
                 FileFolder.nom = nom_fichier
                 FileFolder.save()
 
-                old_file_id = request.session.get('fichier_id', None)
-                if old_file_id is not None:
+                old_file_id = request.session.get('fichier_id', 0)
+                if old_file_id != 0:
                     filename = Fichier.objects.get(pk=old_file_id).nom
 
-                    media_root = getattr(settings, 'MEDIA_ROOT', None)
+                    media_root = getattr(settings, 'MEDIA_ROOT', 0)
                     path_file = os.path.join(media_root, filename)
                     if os.path.isfile(path_file):
                         os.remove(path_file)
@@ -180,17 +323,6 @@ def infos_dataset(dataframe):
     return liste_col
 
 
-class MesFavorisView(TemplateView):
-    template_name = 'pages/projets/mes_favoris.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_white_text'] = False
-        context['section_title'] = 'Mon espace'
-        context['section_item_title'] = 'Mes favoris'
-        return context
-
-
 class MesProjetsView(TemplateView):
     template_name = 'pages/projets/mes_projets.html'
 
@@ -233,227 +365,4 @@ class ConsulterProjetView(TemplateView):
         context['has_white_text'] = False
         context['section_title'] = 'Projets'
         context['section_item_title'] = 'Consultation projet'
-        return context
-
-
-class ModifierProjetView(TemplateView):
-    template_name = 'pages/projets/detail_projet.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_white_text'] = False
-        context['section_title'] = 'Projets'
-        context['section_item_title'] = 'Mise à jour projet'
-        return context
-
-
-class ListeProjetView(TemplateView):
-    template_name = 'pages/projets/liste_projet.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_white_text'] = False
-        context['section_title'] = 'Projets'
-        context['section_item_title'] = 'Listes des projets'
-        return context
-        id = request.POST.get('id', 0)
-        if id == 0:
-            projet = Projet(
-                        title = title,
-                        description = description,
-                        metrique = metrique,
-                        type = type,
-                        est_publique = est_publique,
-                        nombre_modele = nombre_modele,
-                    )
-            projet.save()
-            id = projet.pk
-        else:
-            projet = Projet.objects.get(pk=id)
-            #update projet info
-        
-        request.session['projet_id'] = id
-        return JsonResponse({'data':{'id': id, 'msg':'Information enregistré avec success'}})
-    
-def upload_dataset(request):
-    ts = time.gmtime()
-    ts = time.strftime("__%Y_%m_%d__%H_%M_%S", ts)
-
-    if request.method == 'POST':  
-        fichier = request.FILES['file'].read()
-        nom_fichier = request.POST['filename'] + ts
-        chemin = request.POST['path']
-        end = request.POST['end']
-        nextSlice = request.POST['nextSlice']
-        if fichier=="" or nom_fichier=="" or chemin=="" or end=="" or nextSlice=="":
-            res = JsonResponse({'data':'Requete invalide'})
-            return res
-        else:
-            if chemin == 'null':
-                path = 'media/' + nom_fichier
-                with open(path, 'wb+') as destination: 
-                    destination.write(fichier)
-                FileFolder = Fichier()
-                FileFolder.chemin = nom_fichier 
-                FileFolder.eof = end
-                FileFolder.nom = nom_fichier
-                FileFolder.save()
-
-                old_file_id = request.session.get('fichier_id', None)
-                if old_file_id is not None:
-                    filename = Fichier.objects.get(pk=old_file_id).nom
-
-                    media_root = getattr(settings, 'MEDIA_ROOT', None)
-                    path_file = os.path.join(media_root, filename)
-                    if os.path.isfile(path_file):
-                        os.remove(path_file)
-
-                    Fichier.objects.filter(id=old_file_id).delete()
-                request.session['fichier_id'] = FileFolder.pk
-                
-                if int(end):
-                    data = info_dataset(path)
-                    res = JsonResponse({'msg':'Chargment effectué avec success','data': data, 'chemin': chemin})
-                else:
-                    res = JsonResponse({'chemin': nom_fichier})
-                return res
-
-            else:
-                path = 'media/' + chemin
-                model_id = Fichier.objects.get(chemin=chemin)
-                if model_id.nom == nom_fichier:
-                    if not model_id.eof:
-                        with open(path, 'ab+') as destination: 
-                            destination.write(fichier)
-                        if int(end):
-                            model_id.eof = int(end)
-                            model_id.save()
-                            data = info_dataset(model_id.chemin)
-                            res = JsonResponse({'msg':'Chargement effectué avec success','data': data, 'chemin': model_id.chemin})
-                        else:
-                            res = JsonResponse({'chemin':model_id.chemin})    
-                        return res
-                    else:
-                        res = JsonResponse({'data':'EOF trouvé. Requeste invalide'})
-                        return res
-                else:
-                    res = JsonResponse({'data':'Aucun fichier existant dans ce fichier'})
-                    return res
-    else:
-        res = JsonResponse({'data':'Requete non authorisée'})
-        return res
-
-
-#FONCTION EXTRACTION DES CARACTERISQUE D'UNE COLONNE
-def infos_dataset(dataframe):
-
-    columns = dataframe.columns
-    liste_col = {}
-    for col in columns:
-        macolonne = {}
-        macolonne["type"] = dataframe[col].dtype
-        macolonne["data"] = list(dataframe[col])
-
-        if (dataframe[col].dtype == "int64" or dataframe[col].dtype == "int32"):
-            macolonne["scaler"] = "Standard_Scaler"
-            macolonne["imputer"] = "Mean"
-            macolonne["encoder"] = "None"
-            if dataframe[col].count() < 10:
-                macolonne["nature"] = "discret"
-            else:
-                macolonne["nature"] = "continue"
-
-        elif (dataframe[col].dtype == "bool"):
-            macolonne["scaler"] = "None"
-            macolonne["imputer"] = "Most_frequent"
-            macolonne["encoder"] = "OneHot_Encoder"
-
-            macolonne["nature"] = "discret"
-        else:
-            macolonne["scaler"] = "None"
-            macolonne["imputer"] = "Most_frequent"
-            macolonne["encoder"] = "OneHot_Encoder"
-
-            macolonne["nature"] = "categoriel"
-
-        liste_col[col] = macolonne
-
-    return liste_col
-
-
-class MesFavorisView(TemplateView):
-    template_name = 'pages/projets/mes_favoris.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_white_text'] = False
-        context['section_title'] = 'Mon espace'
-        context['section_item_title'] = 'Mes favoris'
-        return context
-
-
-class MesProjetsView(TemplateView):
-    template_name = 'pages/projets/mes_projets.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_white_text'] = False
-        context['section_title'] = 'Mon espace'
-        context['section_item_title'] = 'Mes Projets'
-        return context
-
-
-class ProjetsPublicsView(TemplateView):
-    template_name = 'pages/projets/projets_publics.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_white_text'] = False
-        context['section_title'] = 'Projets'
-        context['section_item_title'] = 'Projets publiés'
-        return context
-
-
-class NouveauProjetView(TemplateView):
-    template_name = 'pages/projets/creation_projet.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['projet_active'] = True
-        context['has_white_text'] = False
-        context['section_title'] = 'Projets'
-        context['section_item_title'] = 'Creation d\'un projet'
-        return context
-
-
-class ConsulterProjetView(TemplateView):
-    template_name = 'pages/projets/detail_projet.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_white_text'] = False
-        context['section_title'] = 'Projets'
-        context['section_item_title'] = 'Consultation projet'
-        return context
-
-
-class ModifierProjetView(TemplateView):
-    template_name = 'pages/projets/detail_projet.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_white_text'] = False
-        context['section_title'] = 'Projets'
-        context['section_item_title'] = 'Mise à jour projet'
-        return context
-
-
-class ListeProjetView(TemplateView):
-    template_name = 'pages/projets/liste_projet.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_white_text'] = False
-        context['section_title'] = 'Projets'
-        context['section_item_title'] = 'Listes des projets'
         return context
